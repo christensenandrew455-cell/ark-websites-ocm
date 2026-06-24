414457b00a4541b6332f4de4fc81539f7fada085
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-06-24 15:50:15 -0400

    Persist post clients rows

--- app/post-clients/page.js
+++ app/post-clients/page.js
@@ -1,5 +1,5 @@
 import OcmSheet from "../components/OcmSheet";
 
 export default function PostClientsPage() {
-  return <OcmSheet title="Post Clients" />;
+  return <OcmSheet title="Post Clients" storageKey="ark-post-clients" />;
 }
