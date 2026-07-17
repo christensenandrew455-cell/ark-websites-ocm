149ca558ab2f09e3856d2cdcd82be34b9a258be5
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-07-17 14:57:52 -0400

    Expose Firebase messaging service

--- app/lib/firebase-admin.js
+++ app/lib/firebase-admin.js
@@ -1,6 +1,7 @@
 import { cert, getApp, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { getMessaging } from "firebase-admin/messaging";
 
 function getAdminApp() {
   if (getApps().length) return getApp();
@@ -26,6 +27,10 @@ export function getAdminAuth() {
 
 export function getAdminDb() {
   return getFirestore(getAdminApp());
+}
+
+export function getAdminMessaging() {
+  return getMessaging(getAdminApp());
 }
 
 export function getAdminEmails() {
