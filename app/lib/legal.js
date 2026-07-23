b7f557d442307e42372d239c6f5de68328bd2e97
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-07-22 20:23:12 -0400

    Version payment enforcement policies

--- app/lib/legal.js
+++ app/lib/legal.js
@@ -1,3 +1,3 @@
-export const TERMS_VERSION = "2026-07-18";
-export const PRIVACY_VERSION = "2026-07-18";
-export const LEGAL_EFFECTIVE_DATE = "July 18, 2026";
+export const TERMS_VERSION = "2026-07-22";
+export const PRIVACY_VERSION = "2026-07-22";
+export const LEGAL_EFFECTIVE_DATE = "July 22, 2026";
