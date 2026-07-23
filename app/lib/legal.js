b8d40b18b19acf529fab43039c1a3cf31d8e2bfa
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-07-22 23:59:03 -0400

    Version updated account data policies

--- app/lib/legal.js
+++ app/lib/legal.js
@@ -1,3 +1,3 @@
-export const TERMS_VERSION = "2026-07-22";
-export const PRIVACY_VERSION = "2026-07-22";
+export const TERMS_VERSION = "2026-07-22.1";
+export const PRIVACY_VERSION = "2026-07-22.1";
 export const LEGAL_EFFECTIVE_DATE = "July 22, 2026";
